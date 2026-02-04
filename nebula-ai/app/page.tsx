cd32bd58438a3a739926486b50a0b1f500c7db67
binaryshrey <shreyansh.saurabh01@gmail.com>
2026-02-04 01:09:57 -0500

    updates homepage

--- nebula-ai/app/page.tsx
+++ nebula-ai/app/page.tsx
@@ -1,6 +1,12 @@
 import HeroSection from "./components/HeroSection";
 import LogoMarquee from "./components/LogoMarquee";
 import FeatureOne from "./components/FeatureOne";
+import FeatureTwo from "./components/FeatureTwo";
+import FeatureThree from "./components/FeatureThree";
+import FeatureFour from "./components/FeatureFour";
+import FeatureFive from "./components/FeatureFive";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +14,12 @@ export default function Home() {
       <HeroSection />
       <LogoMarquee />
       <FeatureOne />
+      <FeatureTwo />
+      <FeatureThree />
+      <FeatureFour />
+      <FeatureFive />
+      <CTA />
+      <Footer />
     </div>
   );
 }
